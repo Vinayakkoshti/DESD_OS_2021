663f908781d2f30c872340d4f3bc56821e2ade22
vinayak_koshti <vinayakkoshti111@gmail.com>
2021-07-24 21:26:09 +0530

    Demo of fork

---
+++ fork/fork.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+int main()
+{
+    pid_t ID,PID;
+    PID=getpid();
+    printf("Process ID : %d\n",getpid());
+    ID=vfork();
+    if(PID==getpid())
+    {
+    printf("parent ID : %d\n",getpid());
+    }
+    else if (ID==0)
+    {
+        printf("Child ID :%d\n",ID);
+        printf("Process ID : %d\n",getpid());
+    }
+
+        return 0;
+}
